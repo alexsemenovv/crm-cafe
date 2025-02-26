7a411892ae5c4b2f9e27e563ae2c8115be62367e
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-02-26 13:48:30 +0500

    added Dish, Order

--- crm/ordersapp/models.py
+++ crm/ordersapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Dish(models.Model):
+    """
+    Модель Dish представляет блюдо,
+    которое можно заказать в кафе
+
+    Заказы тут: :model:`ordersapp.Order`
+    """
+    class Meta:
+        verbose_name_plural = "dishes"
+
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(null=False, blank=True, db_index=True)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    """
+    Модель Order представляет заказ,
+    в кафе
+    """
+    STATUS_CHOICES = [
+        ("pending", "В ожидании"),
+        ("ready", "Готово"),
+        ("paid", "Оплачено"),
+    ]
+    table_number = models.IntegerField()
+    items = models.ManyToManyField(Dish, related_name='orders')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+
+    def total_price(self):
+        """
+        Вычисляет общую стоимость заказа
+        :return: int
+        """
+        return (dish.price for dish in self.items.objects.all())
+
+    def __str__(self):
+        return f"Заказ {self.id} - Стол {self.table_number} ({self.get_status_display()})"
