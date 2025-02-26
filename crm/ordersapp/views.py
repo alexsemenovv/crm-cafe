22fd85d40bd4f8a76a20c1db27f70d1aa81cab5b
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-02-26 12:45:52 +0500

    added base.html; ch order_index

--- crm/ordersapp/views.py
+++ crm/ordersapp/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def order_index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Hello world!</h1>')
+    """
+    Функция возвращает базовый шаблон при обращении к 'orders/'
+    :param request: HttpRequest - запрос
+    :return: HttpResponse - шаблон в формате html
+    """
+    return render(
+        request,
+        'ordersapp/base.html'
+    )
