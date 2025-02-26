a76ef6159d5205c85e82b41c198f765622796060
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-02-26 17:35:24 +0500

    ch database directory

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATABASE_DIR = BASE_DIR / "database"
+DATABASE_DIR.mkdir(exist_ok=True) # создаём папку в которой будет БД
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,7 +80,7 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_DIR / 'db.sqlite3',
     }
 }
 
