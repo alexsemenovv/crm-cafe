25d8c27fb2d53718655cce6d37733fe36da5d369
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-02-27 12:01:09 +0500

    ch url 'orders' on 'cafe'; fix bugs

--- crm/ordersapp/urls.py
+++ crm/ordersapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import order_index
+from .views import (
+    order_index,
+    DishCreateView,
+)
 
 app_name = "ordersapp"
 
 urlpatterns = [
     path("", order_index, name="index"),
+    path("dishes/create/", DishCreateView.as_view(), name='dish_create'),
 ]
