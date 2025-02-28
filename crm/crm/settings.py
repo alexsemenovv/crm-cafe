21125eabf6978a35762567a6078db9b1a655fec0
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-02-28 16:37:19 +0500

    install drf; configure settings

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'ordersapp.apps.OrdersappConfig',
 ]
 
@@ -125,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
